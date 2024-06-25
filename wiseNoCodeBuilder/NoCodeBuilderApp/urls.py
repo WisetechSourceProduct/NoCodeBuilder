799481047b19c3cc00cb6ff9b2edda934f3ae2c0
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-06-25 17:39:08 +0530

    update

--- wiseNoCodeBuilder/NoCodeBuilderApp/urls.py
+++ wiseNoCodeBuilder/NoCodeBuilderApp/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path("sample/", views.sample, name="samplePage"), # Now it's placed in `about us` page but Need to remove after complete development
-    path("editing/", views.editing, name="editPage"),
+    path("website_editor/", views.website_editor, name="website_editor_page"), # Now it's placed in `about us` page but Need to remove after complete development
     path('save_pages/', views.save_pages, name='save_pages'),
     path('submit-form/', views.handle_form_submission, name='handle_form_submission')
 ]
